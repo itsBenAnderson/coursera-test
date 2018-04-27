035b9340f1e568e17526142579ad191b17018e54
itsBenAnderson <itsbenanderson@gmail.com>
2018-04-27 09:22:22 -0400

    Add files via upload

---
+++ angular/assignment1-solution/app.js
@@ -0,0 +1,29 @@
+(function () {
+
+    angular.module('lunchChecker', [])
+        .controller('lunchController', lunchController);
+    lunchController.$inject = ['$scope'];
+    function lunchController($scope) {
+        $scope.message = "";
+        $scope.showMessage = function () {
+            var dishes;
+            if ($scope.input) {
+                $scope.input = $scope.input.trim();
+                dishes = $scope.input.split(",");
+                dishes = dishes.filter(function (item) {
+                    return item.length > 0;
+                });
+                if (dishes.length <= 3) {
+                    $scope.message = "Enjoy!";
+                }
+                else {
+                    $scope.message = "Too Much!";
+                }
+            }
+            else {
+                $scope.message = "What did you have for lunch?";
+            }
+        };
+    }
+    
+})();
