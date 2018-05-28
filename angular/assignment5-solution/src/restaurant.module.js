3b66d0bece3d9637e9a3a634c7ad284a2bc49571
itsBenAnderson <itsbenanderson@gmail.com>
2018-05-27 22:20:31 -0400

    Add files via upload

---
+++ angular/assignment5-solution/src/restaurant.module.js
@@ -0,0 +1,9 @@
+(function () {
+    "use strict";
+    angular.module('restaurant', ['public'])
+        .config(config);
+    config.$inject = ['$urlRouterProvider'];
+    function config($urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+    }
+})();
